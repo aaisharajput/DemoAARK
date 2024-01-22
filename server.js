2c61e8d2c4f6050ef7fb2874b52d75265b719d7a
aaisharajput <sapnadevi@aarkglobalinc.com>
2024-01-22 21:14:27 +0530

    bb

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 import express from "express";
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const port =3000;
  
 app.use(express.static('./View/dist'));
-
+app.get('/',(req,res)=>{
+  res.send("welcome to aark server")
+})
 app.get('*', (req, res) => {
-    res.sendFile(resolve(__dirname, '../','View', 'dist', 'index.html'));
+    res.sendFile(resolve(__dirname,'View', 'dist', 'index.html'));
   });
 
 
